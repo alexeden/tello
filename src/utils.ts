3d1687eb0b4d83687b960775bed078d115fac2be
Alex Eden <alexandereden91@gmail.com>
2018-09-19 01:44:21 -0500

    Add  a sample argument to the state parser comments

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ import { TelloState } from './types';
 
 export class TelloUtils {
   // “pitch:%d;roll:%d;yaw:%d;vgx:%d;vgy%d;vgz:%d;templ:%d;temph:%d;tof:%d;h:%d;bat:%d;baro:%.2f;time:%d;agx:%.2f;agy:%.2f;agz:%.2f;\r\n”
+  /**
+   * Sample argument:
+   * pitch:-1;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:75;temph:76;tof:10;h:0;bat:79;baro:266.36;time:0;agx:-17.00;agy:-8.00;agz:-991.00;
+   */
   static parseState(rawState: string): Readonly<TelloState> {
     const emptyState: TelloState = {
       tof: -1,
