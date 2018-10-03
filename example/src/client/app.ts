bea72a7ff5daeb27a0ec28016b9fcc1a0cfad896
Alex Eden <alexandereden91@gmail.com>
2018-10-03 01:04:21 -0500

    Integrate a stat listener for tracking performance

--- example/src/client/app.ts
+++ example/src/client/app.ts
@@ -3,27 +3,19 @@ import { Player } from './player';
 const h264Player = new Player({
   workerFile : 'Decoder.js',
   size: { width: 1280, height: 720 },
+  statsListener: stats => (window as any).stats = stats,
 });
 
 
 document.getElementById('videoFeed')!.appendChild(h264Player.canvas);
 
-const toUint8Array = (str: string) => {
-  const array = new Uint8Array(new ArrayBuffer(str.length));
-  let i;
-  for (i = 0; i < str.length; i++) {
-    array[i] = str.charCodeAt(i);
-  }
-  return array;
-};
-
 try {
   const videoSocket = new WebSocket(`wss://${window.location.host}/video`);
   videoSocket.onerror = () => videoSocket.close();
-  videoSocket.onmessage = imgString =>
-    imgString.data !== 'false'
-      ? h264Player.decode(toUint8Array(imgString.data))
-      : console.log(imgString);
+  videoSocket.onmessage = e =>
+    e.data !== 'false'
+      ? h264Player.decode(e.data, { startProcessing: performance.now() })
+      : console.log(e);
 }
 catch (e) {
   console.log('video socket error: ', e);
--- example/src/client/player.ts
+++ example/src/client/player.ts
@@ -7,6 +7,7 @@ interface Size {
 interface PlayerOptions {
   size?: Size;
   workerFile?: string;
+  statsListener?: (stats: PlayerStats) => void;
 }
 
 interface Frame extends Size {
@@ -14,12 +15,28 @@ interface Frame extends Size {
   infos: any;
 }
 
+interface PlayerStats {
+  processing: { start: number, finish: number };
+  decoding: { start: number, finish: number };
+  rendering: { start: number, finish: number };
+}
+
+type DecoderMessage
+  = { consoleLog: string }
+  | {
+      buf: ArrayBuffer;
+      length: number;
+      height: number;
+      width: number;
+      infos: Array<{ startDecoding: number, finishDecoding: number }>
+    };
+
 export class Player {
   private readonly worker: Worker;
   private readonly webGLCanvas: WebGLCanvas;
   readonly workerFile: string;
   readonly canvas: HTMLCanvasElement;
-
+  statsListener: (stats: PlayerStats) => void;
   nowValue: number;
   size: Size;
 
@@ -28,6 +45,8 @@ export class Player {
 
     this.workerFile = options.workerFile || 'Decoder.js';
 
+    this.statsListener = options.statsListener || (() => { /* no-op */ });
+
     this.size = {
       width: options.size && options.size.width || 200,
       height: options.size && options.size.height || 200,
@@ -35,34 +54,10 @@ export class Player {
 
     this.canvas = Player.createCanvas(this.size);
 
-    this.webGLCanvas = new WebGLCanvas({
-      canvas: this.canvas,
-      ...this.size,
-    });
-
-    // provide size
+    this.webGLCanvas = new WebGLCanvas({ canvas: this.canvas, ...this.size });
     this.worker = new Worker(this.workerFile);
-    this.worker.addEventListener('message', e => {
-      (window as any).workerMessageEvent = e;
-      const data = e.data;
-      if (data.consoleLog) {
-        console.log(data.consoleLog);
-        return;
-      }
-      if (!data.buf || data.buf.length < 1) {
-        console.log('empty buffer');
-        return;
-      }
-      const buffer = new Uint8Array(data.buf, 0, data.length);
-      const frame = {
-        buffer,
-        height: data.height,
-        width: data.width,
-        infos: data.infos,
-      };
-      this.renderFrame(frame);
-      (window as any).frame = frame;
-    });
+
+    this.worker.addEventListener('message', this.handleWorkerMessage.bind(this));
 
     this.worker.postMessage({
       type: 'Broadway.js - Worker init',
@@ -74,7 +69,46 @@ export class Player {
     });
   }
 
-  decode(data: Uint8Array, info: any = {}) {
+  handleWorkerMessage(e: MessageEvent) {
+    (window as any).workerMessageEvent = e;
+    const data: DecoderMessage = e.data;
+
+    if ('consoleLog' in data) {
+      console.log(data.consoleLog);
+      return;
+    }
+
+    if (!data.buf) {
+      console.log('empty buffer');
+      return;
+    }
+
+    const renderStats = this.renderFrame({
+      buffer: new Uint8Array(data.buf, 0, data.length),
+      height: data.height,
+      width: data.width,
+      infos: data.infos,
+    });
+
+    const info = data.infos.reduce((accum, i: any) => ({ ...accum, ...i }), {});
+
+    this.statsListener({
+      processing: {
+        start: info.startProcessing,
+        finish: performance.now(),
+      },
+      rendering: renderStats,
+      decoding: {
+        start: info.startDecoding || -1,
+        finish: info.finishDecoding || -1,
+      },
+    });
+  }
+
+  decode(data: string | Uint8Array, info: any = {}) {
+    if (typeof data === 'string') {
+      data = Player.toUint8Array(data);
+    }
     // Copy the sample so that we only do a structured clone of the region of interest
     const copyU8 = new Uint8Array(data.length);
     copyU8.set(data, 0);
@@ -87,6 +121,15 @@ export class Player {
     this.worker.postMessage(message, [copyU8.buffer]);
   }
 
+  static toUint8Array(str: string) {
+    const array = new Uint8Array(new ArrayBuffer(str.length));
+    let i;
+    for (i = 0; i < str.length; i++) {
+      array[i] = str.charCodeAt(i);
+    }
+    return array;
+  }
+
   static createCanvas({ height, width }: Size, bgColor = '#0D0E1B'): HTMLCanvasElement {
     const canvas = document.createElement('canvas');
     canvas.width = width;
@@ -96,6 +139,7 @@ export class Player {
   }
 
   private renderFrame({ buffer, height, width }: Frame) {
+    const start = performance.now();
     const ylen = width * height;
     const uvlen = (width / 2) * (height / 2);
 
@@ -104,5 +148,10 @@ export class Player {
       uData: buffer.subarray(ylen, ylen + uvlen),
       vData: buffer.subarray(ylen + uvlen, ylen + uvlen + uvlen),
     });
+
+    return {
+      start,
+      finish: performance.now(),
+    };
   }
 }
