4ad3ce32db1e73bba7b643a8b9ab7f80e402c7a6
Alex Eden <alexandereden91@gmail.com>
2018-10-07 00:53:32 -0500

    Start building an actual primitive UI

--- example/src/client/app.ts
+++ example/src/client/app.ts
@@ -5,6 +5,7 @@ import { Subscription, pipe } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { retryBackoff } from 'backoff-rxjs';
 import { Player } from './player';
+import { error } from 'util';
 
 // const h264Player = new Player({
 //   size: { width: 1280, height: 720 },
@@ -27,8 +28,22 @@ import { Player } from './player';
 
 
 const template = `
-<div>
+<div class="column gap-10">
   <h1>Hello, Tello</h1>
+  <div class="column">
+    <div class="row align-center gap-10">
+      <p>Video Socket</p>
+      <span class="c-green" v-if="videoConnected">Connected</span>
+      <span class="c-alternate" v-else>Disconnected</span>
+      <button v-if="!videoConnected" @click="connectVideo">Reconnect</button>
+    </div>
+    <div class="row align-center gap-10">
+      <p>State Socket</p>
+      <span class="c-green" v-if="stateConnected">Connected</span>
+      <span class="c-alternate" v-else>Disconnected</span>
+      <button v-if="!stateConnected" @click="connectState">Reconnect</button>
+    </div>
+  </div>
   <p v-if="stateSocket.isStopped">Socket is stopped</p>
   <pre>{{ state }}</pre>
   <div ref="videoWrapper"></div>
@@ -39,45 +54,87 @@ new Vue({
   el: '#app',
   template,
   data() {
-    const rect = document.body.getBoundingClientRect();
-
     return {
       state: null as null | any,
-      videoFrame: 0,
-      stateSocket: new WebSocketSubject(`wss://${window.location.host}/state`),
+      stateSocket: new WebSocket(`wss://${window.location.host}/state`),
+      // stateSocket: new WebSocketSubject(`wss://${window.location.host}/state`),
+      stateConnected: false,
+      videoConnected: false,
       videoSocket: new WebSocket(`wss://${window.location.host}/video`),
       subscriptions: [] as Subscription[],
-      player: new Player({
-        size: { width: 1280, height: 720 },
-        // size: { width: rect.width, height: (rect.width * 9) / 16 },
-        statsListener: stats => (window as any).stats = stats,
-      }),
+      player: new Player({ size: { width: 1280, height: 720 }}),
     };
+  },
+  created() {
+    this.connectVideo();
   },
   mounted() {
     (window as any).app = this;
     const videoWrapper = this.$refs.videoWrapper as HTMLDivElement;
     videoWrapper.appendChild(this.player.canvas);
+    // this.videoSocket.onerror = () => this.videoSocket.close();
+    // this.videoSocket.onmessage = e => this.player.decode(e.data, { startProcessing: performance.now() });
 
-    this.videoSocket.onerror = () => this.videoSocket.close();
-    this.videoSocket.onmessage = e => this.player.decode(e.data, { startProcessing: performance.now() });
-
-    // const videoSubscription = this.videoSocket.asObservable()
+    // const stateSubscription = this.stateSocket.asObservable()
     //   .pipe(retryBackoff({ initialInterval: 1000 }))
     //   .subscribe(
-    //     data => this.player.decode(data, { startProcessing: performance.now() }),
-    //     err => console.error('Video stream error: ', err)
+    //     msg => this.state = msg,
+    //     error => console.error('State stream error: ', error)
     //   );
 
-    const stateSubscription = this.stateSocket.asObservable()
-      .pipe(retryBackoff({ initialInterval: 1000 }))
-      .subscribe(
-        msg => this.state = msg,
-        error => console.error('State stream error: ', error)
-      );
 
-
-    this.subscriptions.push(stateSubscription);
-    // this.subscriptions.push(videoSubscription);
+    // this.subscriptions.push(stateSubscription);
+  },
+  beforeDestroy() {
+    let sub;
+    // tslint:disable-next-line:no-conditional-assignment
+    while (sub = this.subscriptions.shift()) {
+      sub.unsubscribe();
+    }
+  },
+  computed: {
+    // stateConnected(): boolean {
+    //   return this.stateSocket.readyState !== WebSocket.OPEN;
+    // },
+    // videoConnected(): boolean {
+    //   return this.videoSocket.readyState !== WebSocket.OPEN;
+    // },
+  },
+  methods: {
+    connectState() {
+      console.log('connecting state');
+      const stateSocket = this.stateSocket.readyState !== WebSocket.OPEN && this.stateSocket.readyState !== WebSocket.CONNECTING
+        ? new WebSocket(`wss://${window.location.host}/state`)
+        : this.stateSocket;
+      const { readyState } = stateSocket;
+      this.stateConnected = readyState === WebSocket.OPEN;
+      stateSocket.onopen = () => this.stateConnected = true;
+      stateSocket.onerror = errorEvent => {
+        console.error('state socket error', errorEvent);
+        this.stateConnected = false;
+      };
+      stateSocket.onclose = () => this.stateConnected = false;
+      stateSocket.onmessage = e => {
+        this.state = e.data;
+      };
+      this.stateSocket = stateSocket;
+    },
+    connectVideo() {
+      const { readyState } = this.videoSocket;
+      const videoSocket = readyState !== WebSocket.OPEN && readyState !== WebSocket.CONNECTING
+        ? new WebSocket(`wss://${window.location.host}/video`)
+        : this.videoSocket;
+      this.videoConnected = readyState === WebSocket.OPEN;
+      videoSocket.onopen = () => this.videoConnected = true;
+      videoSocket.onerror = errorEvent => {
+        console.error('video socket error', errorEvent);
+        this.videoConnected = false;
+      };
+      videoSocket.onclose = () => {
+        this.videoConnected = false;
+      };
+      videoSocket.onmessage = e => this.player.decode(e.data, { startProcessing: performance.now() });
+      this.videoSocket = videoSocket;
+    },
   },
 });
