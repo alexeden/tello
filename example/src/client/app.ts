704cd43ab73d64923b4977099a69ec9a45e71bbf
Alex Eden <alexandereden91@gmail.com>
2018-10-10 16:59:38 -0500

    Setup an observable that generates stick data based on keyboard keys

--- example/src/client/app.ts
+++ example/src/client/app.ts
@@ -1,20 +1,69 @@
 // tslint:disable no-invalid-this
 import Vue from 'vue';
-import { Subscription, pipe } from 'rxjs';
+import VueRx from 'vue-rx';
+import { Subscription, pipe, merge, fromEvent } from 'rxjs';
 import { Player } from './player';
 import * as template from './app.html';
+import { filter, scan, map } from 'rxjs/operators';
+
+interface Status {
+  flying: boolean;
+  onGround: boolean;
+  hovering: boolean;
+  batteryLow: boolean;
+  batteryVeryLow: boolean;
+}
+
+enum Controls {
+  Forward = 'KeyW',
+  Left = 'KeyA',
+  Backward = 'KeyS',
+  Right = 'KeyD',
+  RotateCCW = 'KeyJ',
+  RotateCW = 'KeyK',
+}
+
+type ControlKeyMap = {
+  [P in Controls]: 0 | 1
+};
+
+interface RemoteControl {
+  fastMode: boolean;
+  leftX: number;
+  leftY: number;
+  rightX: number;
+  rightY: number;
+}
+
+
+
+Vue.use(VueRx);
 
 new Vue({
   el: '#app',
   template,
   data() {
+    const keycodes = Object.values(Controls);
+    const keymapInit = keycodes.reduce((keymap, k) => ({ ...keymap, [k]: 0 }), {}) as ControlKeyMap;
+
     return {
       player: new Player({ size: { width: 1280, height: 720 }}),
       state: {} as any,
+      keymap: merge(fromEvent<KeyboardEvent>(document, 'keydown'), fromEvent<KeyboardEvent>(document, 'keyup')).pipe(
+        filter(e => !e.repeat && keycodes.includes(e.code)),
+        scan<KeyboardEvent, ControlKeyMap>((keymap, e) => ({ ...keymap, [e.code]: ~~(e.type === 'keydown') }), keymapInit)
+      ),
       stateSocket: null as (null | WebSocket),
       videoSocket: null as (null | WebSocket),
       stateConnected: false,
       videoConnected: false,
+      remoteControl: {
+        fastMode: false,
+        leftX: 0,
+        leftY: 0,
+        rightX: 0,
+        rightY: 0,
+      } as RemoteControl,
       subscriptions: [] as Subscription[],
     };
   },
@@ -22,10 +71,22 @@ new Vue({
     (window as any).app = this;
     this.connectState();
     // this.connectVideo();
+
   },
   mounted() {
     const videoWrapper = this.$refs.videoWrapper as HTMLDivElement;
     videoWrapper.appendChild(this.player.canvas);
+
+    this.keymap.pipe(
+      map<ControlKeyMap, RemoteControl>(keymap => ({
+        fastMode: false,
+        leftX: (-1 * keymap[Controls.RotateCCW]) + keymap[Controls.RotateCW],
+        leftY: 0,
+        rightX: (-1 * keymap[Controls.Left]) + keymap[Controls.Right],
+        rightY: (-1 * keymap[Controls.Backward]) + keymap[Controls.Forward],
+      }))
+    )
+    .subscribe(console.log);
   },
   beforeDestroy() {
     let sub;
@@ -41,6 +102,21 @@ new Vue({
     battery(): any {
       return this.state && this.state.battery || {};
     },
+    status(): Status {
+      return {
+        flying: false,
+        onGround: false,
+        hovering: false,
+        batteryLow: false,
+        batteryVeryLow: false,
+        ...(this.state && this.state.status || {}),
+      };
+    },
+  },
+  subscriptions() {
+
+    return {
+    };
   },
   methods: {
     send(command: number, data: any = null) {
